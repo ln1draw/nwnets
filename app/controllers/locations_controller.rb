c9bf4583c7d8b94a6f6d3cd16d163900b0f30b64
ellen wondra <ellenfromillinois@gmail.com>
2019-08-27 16:27:20 -0700

    search has at least initial functionality

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,14 @@
 class LocationsController < ApplicationController
   def index
     @locations = Location.all
+
+    a = params["address"]
+    @locations = Location.near(a) unless a.nil?
+
+    if @locations.length == 0
+      @locations = Location.all
+      flash[:notice] = "Unable to find results; please check address"
+    end
   end
 
   def show
